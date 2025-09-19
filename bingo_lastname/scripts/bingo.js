830e4c99b0b5ad79f4c5ac4cac19a941bfa93888
MEDRANO, Dave H. <evadonardem@gmail.com>
2025-09-19 16:31:45 +0800

    Updated BINGO game template.

--- bingo_lastname/scripts/bingo.js
+++ bingo_lastname/scripts/bingo.js
@@ -1,1 +1,14 @@
-// Add your bingo game JavaScript code here
+function generateBingoCards(count) {
+    const cardsPlaceholderElement = document.getElementById('cardsPlaceholder');
+    cardsPlaceholderElement.innerHTML = "";
+
+    // do loop to create the cards
+    for (let i = 0; i < count; i++) {
+        cardsPlaceholderElement.innerHTML += `Card${i}`;
+    }
+}
+
+document.getElementById('numberOfCards').addEventListener('change', (event) => {
+    const numberOfCards = event.target.value;
+    generateBingoCards(numberOfCards);
+});
